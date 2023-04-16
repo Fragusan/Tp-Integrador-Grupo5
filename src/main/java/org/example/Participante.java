4ca968e997777d51b0d0b26871510603f8cdf99b
fragusan <fragusan2@gmail.com>
2023-04-16 17:44:41 -0300

    metodo de calcular puntaje en participante

--- src/main/java/org/example/Participante.java
+++ src/main/java/org/example/Participante.java
@@ -6,10 +6,21 @@ import java.util.List;
 @Data
 public class Participante {
     private  String nombre;
-    private int puntos ;
+    private int puntaje ;
     private Resultado apuesta;
     private List<Pronostico> pronosticoParticipante;
 
+	public int calcularPuntaje(List<Partido> partidos) {
+        int puntaje = 0;
+        for (Partido partido : partidos) {
+            if (partido.getResultado() == this.apuesta) {
+                puntaje += 3;
+            } else if (partido.getResultado() == Resultado.EMPATE && this.apuesta == Resultado.EMPATE) {
+                puntaje += 1;
+            }
+        }
+        return puntaje;
+    }
 
       /* pronosticoParticipante = new ArrayList<>();
         for (Pronostico p : pronosticoParticipante) {
